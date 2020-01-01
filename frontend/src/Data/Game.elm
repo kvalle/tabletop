0ba66ac351b3c10e13e38014144cdd7d0768d966
Kjetil Valle <kjetil.valle@gmail.com>
2020-01-02 00:14:55 +0100

    Simple display logic

--- frontend/src/Data/Game.elm
+++ frontend/src/Data/Game.elm
@@ -5,10 +5,12 @@ import Json.Decode as Decode exposing (Decoder)
 
 type alias Game =
     { id : String
+    , title : String
     }
 
 
 decoder : Decoder Game
 decoder =
-    Decode.map Game <|
-        Decode.field "id" Decode.string
+    Decode.map2 Game
+        (Decode.field "id" Decode.string)
+        (Decode.field "title" Decode.string)
--- frontend/src/View.elm
+++ frontend/src/View.elm
@@ -1,5 +1,7 @@
 module View exposing (view)
 
+import Data.Game as Game exposing (Game)
+import Data.GamesRequest exposing (GamesRequest(..))
 import Html exposing (..)
 import Messages exposing (Msg)
 import Model exposing (Model)
@@ -12,7 +14,24 @@ view model =
             [ text "Board game collection of BGG user "
             , em [] [ text model.username ]
             ]
-        , p []
-            [ text "Loading"
-            ]
+        , case model.gamesRequest of
+            Success games ->
+                ul [] <|
+                    List.map displayGame games
+
+            BggProcessing ->
+                p [] [ text "Waiting for BGG to process the games" ]
+
+            Requesting ->
+                p [] [ text "Loading" ]
+
+            Error _ ->
+                p [] [ text "Something went wrong :(" ]
+        ]
+
+
+displayGame : Game -> Html Msg
+displayGame game =
+    li []
+        [ text game.title
         ]
